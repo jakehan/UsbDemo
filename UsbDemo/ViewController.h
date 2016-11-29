ff132633f58fb5b15508dbf8e020b00fb8fdaf4f
xuefei <xuefei@xuefeidemac-mini.local>
2016-11-29 13:18:14 +0800

    Initial Commit

---
+++ UsbDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UsbDemo
+//
+//  Created by xuefei on 16/11/29.
+//  Copyright © 2016年 xuefei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UsbDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UsbDemo
+//
+//  Created by xuefei on 16/11/29.
+//  Copyright © 2016年 xuefei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
